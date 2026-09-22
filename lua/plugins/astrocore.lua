25ccbb4bbb15ecb0c168de3456611d81741346e2
OpalBSD <nawfalmrouyan@gmail.com>
2026-09-22 23:03:33 +0800

    L and H uses default keybind instead of traversing buffers

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -102,8 +102,8 @@ return {
       n = {
         -- second key is the lefthand side of the map
         -- navigate buffer tabs with `H` and `L`
-        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        -- L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        -- H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
         -- navigate buffer tabs
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
